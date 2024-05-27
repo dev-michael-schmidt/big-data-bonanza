7291626b34336774bb63ee0e5fba0c52cd899502
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-05-26 22:49:12 -0500

    skeleton for XZ

--- xz_handler.py
+++ xz_handler.py
@@ -1,1 +1,15 @@
 from abstracts import Archive
+
+class XZHandler(Archive):
+
+    def __init__(self):
+        super().__init__()
+
+    def extract(self):
+        pass
+
+    def compress(self):
+        pass
+
+    def transform(self):
+        pass
