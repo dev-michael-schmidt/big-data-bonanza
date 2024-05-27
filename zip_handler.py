ad73f0edce8253931f6952e1739ca74644fa8a25
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-05-26 22:38:47 -0500

    define zip functions using 'tar' file and 'tempfile'

--- zip_handler.py
+++ zip_handler.py
@@ -1,25 +1,24 @@
-
-
 import os
 import tempfile
 import zipfile
 import tarfile
 
+from abstracts import Archive
 from constants import (XZ_SUFFIX, ZIP_SUFFIX)
 
 
-class ZipHandler:
+class ZipHandler(Archive):
 
     SRC_FOLDER = os.getcwd()
     DEST_PATH = os.path.join(os.getcwd(), SRC_FOLDER)
 
     def __init__(self):
         pass
-    def
+    def extract(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with zipfile.ZipFile(f'{SRC_FOLDER}/{INCOMING_NAME}.{ZIP_SUFFIX}') as _zip:
+                _zip.extractall(tmpdir)
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with zipfile.ZipFile(f'{SRC_FOLDER}/{INCOMING_NAME}.{ZIP_SUFFIX}') as _zip:
-            _zip.extractall(tmpdir)
-
+    def compress(self):
         with tarfile.open(f'{DEST_PATH}/{DESTINATION_NAME}.{XZ_SUFFIX}', mode='w:xz') as _tar:
             _tar.add(tmpdir)
